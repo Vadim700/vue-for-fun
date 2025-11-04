0d1549d5b71d96d8e8c3006a987de24ea0524e77
VadimBekmansurov <bekmansurov700@mail.ru>
2025-11-04 21:23:18 +0500

    Added animation for madal

--- src/components/modals/vModal.vue
+++ src/components/modals/vModal.vue
@@ -1,27 +1,39 @@
 <template>
   <Teleport to="body">
     <div
+      ref="modal"
       v-if="open"
       class="w-full h-full grid absolute top-0 left-0 before:absolute before:bg-black before:w-full before:h-full before:opacity-80 before:backdrop-blur-[5px]"
-      @click="handleBackdropClick"
+      @click="handleOutsideClick"
     >
-      <div
-        class="border w-full max-w-[500px] min-w-[350px] absolute left-[50%] top-[50%] -translate-[50%] bg-white px-6 py-4 pt-8"
-        ref="modalInner"
-        @click.stop
+      <motion.div
+        :initial="{ opacity: 0, scale: 0.6, translateY: 50 }"
+        :animate="{ opacity: 1, scale: 1, translateY: 0 }"
+        :exit="{ opacity: 0, scale: 0.6 }"
+        :transition="{
+          duration: 0.2,
+          delay: 0,
+          ease: 'easeOut',
+        }"
       >
-        <button
-          class="absolute top-2 right-2 cursor-pointer hover:text-red-500"
-          @click="$emit('close')"
+        <div
+          class="border w-full max-w-[500px] min-w-[350px] absolute left-[50%] top-[50%] -translate-[50%] bg-white px-6 py-4 pt-8"
+          ref="modalInner"
+          @click.stop
         >
-          <X />
-        </button>
-        <h3 class="text-3xl text-center mb-4">
-          <slot name="header"></slot>
-        </h3>
-        <slot></slot>
-        <slot name="footer"></slot>
-      </div>
+          <button
+            class="absolute top-2 right-2 cursor-pointer hover:text-red-500"
+            @click="closeModal()"
+          >
+            <X />
+          </button>
+          <h3 class="text-3xl text-center mb-4">
+            <slot name="header"></slot>
+          </h3>
+          <slot></slot>
+          <slot name="footer"></slot>
+        </div>
+      </motion.div>
     </div>
   </Teleport>
 </template>
@@ -29,6 +41,7 @@
 <script setup>
 import { X } from 'lucide-vue-next'
 import { onMounted, onUnmounted, ref } from 'vue'
+import { motion } from 'motion-v'
 
 const emit = defineEmits(['close'])
 const modalInner = ref(null)
@@ -36,16 +49,21 @@ const props = defineProps({
   open: Boolean,
 })
 
-const handleBackdropClick = (event) => {
+const handleOutsideClick = (event) => {
   if (event.target === event.currentTarget) {
-    emit('close')
+    closeModal()
   }
 }
 
 const handleKeydown = (event) => {
   if (event.key === 'Escape' && props.open) {
-    emit('close')
+    closeModal()
   }
+}
+
+const closeModal = () => {
+  emit('close')
+  // setTimeout(() => emit('close'), 1000)
 }
 
 onMounted(() => document.addEventListener('keydown', handleKeydown))
