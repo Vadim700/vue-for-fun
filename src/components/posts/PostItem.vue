48c63be1b5ca1b22a412dce4a158d641334e3956
VadimBekmansurov <bekmansurov700@mail.ru>
2025-12-23 01:02:59 +0500

    added images for posts page

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,43 +1,15 @@
 <template>
-  <slot>
-    <div>
-      <div class="root rounded-sm p-2 relative min-h-[200px]">
-        <button
-          @click="deletePost(post.id)"
-          class="absolute right-2 top-2 cursor-pointer hover:text-red-500 hover:rotate-90 transition-all ease-in-out duration-200"
-        >
-          <X />
-        </button>
-        <h3
-          class="text-2xl mb-2 truncate overflow-hidden whitespace-nowrap pr-10 first-letter:uppercase"
-        >
-          {{ post.title }}
-        </h3>
-
-        <p class="first-letter:uppercase">{{ post.body }}</p>
-      </div>
-    </div>
-  </slot>
+  <img :src="getImageSrc(imageName)" alt="Dynamic image" />
 </template>
 
-<script setup>
-import { X } from 'lucide-vue-next'
-import { usePostsStore } from '@/stores/postsStore'
-const { deletePost } = usePostsStore()
-
-const props = defineProps({
-  post: {
-    type: Object,
-    required: false,
-    default: () => {},
+<script>
+export default {
+  props: ['imageName'],
+  methods: {
+    getImageSrc(name) {
+      // Возвращает путь к изображению из assets
+      return require(`@/assets/products/${name}.jpg`)
+    },
   },
-})
-</script>
-
-<style lang="scss" scoped>
-.root {
-  -webkit-box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
-  -moz-box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
-  box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
 }
-</style>
+</script>
