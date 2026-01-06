5147ed7ca69d2ebc47188d83dda050f84f16ac04
VadimBekmansurov <bekmansurov700@mail.ru>
2026-01-06 14:44:39 +0500

    save topic after reload page

--- src/App.vue
+++ src/App.vue
@@ -8,31 +8,32 @@
 <script setup>
 import Header from '@/components/vHeader.vue'
 import { useMainStore } from './stores/main'
-import { watch, ref } from 'vue'
+import { watch, ref, onMounted } from 'vue'
 
 const store = useMainStore()
 const bgColor = ref('')
 
-// Вариант 1: Использование watch (рекомендуется)
 watch(
   () => store.topic,
-  (newTopic) => {
-    switch (newTopic) {
-      case 'posts':
-        bgColor.value = 'linear-gradient(to right bottom, #04a561, #01703e)'
-        break
-      case 'todos':
-        bgColor.value = 'linear-gradient(to right bottom, #5EC6C7, #066f72)'
-        break
-      case 'onlycss':
-        bgColor.value = 'linear-gradient(to right bottom, #5c6bc0, #42a5f5)'
-        break
-      default:
-        bgColor.value = ''
-    }
-  },
+  (newTopic) => setBg(newTopic),
   { immediate: true },
 )
+
+function setBg(topic) {
+  switch (topic) {
+    case 'posts':
+      bgColor.value = 'linear-gradient(to right bottom, #04a561, #01703e)'
+      break
+    case 'todos':
+      bgColor.value = 'linear-gradient(to right bottom, #5EC6C7, #066f72)'
+      break
+    case 'onlycss':
+      bgColor.value = 'linear-gradient(to right bottom, #5c6bc0, #42a5f5)'
+      break
+    default:
+      bgColor.value = ''
+  }
+}
 </script>
 <style lang="scss">
 html,
